661e833d1f7e5bf1a131008ed535051f04b53bbc
Alexandr <kataisk2002@gmail.com>
2022-12-24 02:31:12 +0500

    upd

--- index.php
+++ index.php
@@ -1,24 +1,22 @@
 <?php
-  $email = ' ';
-  //echo "<br> " . print_r($_POST['email']);
-  if (empty($_POST['email']) == 0) {
+$email = ' ';
+//echo "<br> " . print_r($_POST['email']);
+if (empty($_POST['email']) == 0) {
     if (isset($_POST['email'])) {
-      $email = $_POST['email'];
+        $email = $_POST['email'];
     }
     $email = stripslashes($email);
     $email = htmlspecialchars($email);
     $email = trim($email);
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $error_with_email = false;
+        $error_with_email = false;
+    } else {
+        $error_with_email = true;
     }
-    else {
-      $error_with_email = true;
-    }
-  }
-  else {
+} else {
     $error_with_email = false;
     $email = ' ';
-  }
+}
 ?>
 
 <html lang="ru">
@@ -26,60 +24,56 @@
 <meta charset="utf-8">
 
 <head>
-  <title>Регистрация</title>
-  <link rel="stylesheet" href="index.css" />
+    <title>Регистрация</title>
+    <link rel="stylesheet" href="index.css"/>
 </head>
 
 <body>
-  <h2>Регистрация</h2>
-  <form 
-    <?php
-      if ($error_with_email) {
-        echo "action=\"save_user.php\"";
-      }
-      else {
-        echo "action=\"index.php\"";
-      }
-    ?> method="post">
-    <!--**** save_user.php - это адрес обработчика.  То есть, после нажатия на кнопку "Зарегистрироваться", данные из полей  отправятся на страничку save_user.php методом "post" ***** -->
-    <p>
-      <label>Ваше имя:<br></label>
-      <input name="login" type="text" size="15" maxlength="15">
+<div class="card">
+    <p class="text">
+        Регистрация
     </p>
-    <!--**** В текстовое поле (name="login" type="text") пользователь вводит свой логин ***** -->
-    <p>
-      <label>Ваш e-mail:<br></label>
-      <input name="email" type="text"
-      <?php
-        if (empty($_POST['email'] == 1)) {
-          echo "style=\"border-color: black;\"";
+    <form
+        <?php
+        if ($error_with_email) {
+            echo "action=\"save_user.php\"";
+        } else {
+            echo "action=\"index.php\"";
         }
-        else {
-          if (!$error_with_email) {
-            echo "style=\"border-color: red;\"";
-          }
-          else {
-            echo "style=\"border-color: black;\"";
-          }
-        }
-      ?>
-      size="15" maxlength="30">
-      <?php
+        ?> method="post">
+        <!--**** save_user.php - это адрес обработчика.  То есть, после нажатия на кнопку "Зарегистрироваться", данные из полей  отправятся на страничку save_user.php методом "post" ***** -->
+        <div class="item">
+            <input class="field" name="login" type="text" size="15" maxlength="15" placeholder="Ваше имя">
+        </div>
+        <!--**** В текстовое поле (name="login" type="text") пользователь вводит свой логин ***** -->
+        <div class="item">
+            <input class="field" name="email" type="text" <?php
+            if (empty($_POST['email'] == 1)) {
+                echo "style=\"border-color: black;\"";
+            } else {
+                if (!$error_with_email) {
+                    echo "style=\"border-color: red;\"";
+                } else {
+                    echo "style=\"border-color: black;\"";
+                }
+            }
+            ?> size="15" maxlength="30" placeholder="Ваш e-mail">
+        </div>
+        <?php
         if (!$error_with_email) {
-          echo "<br>Неверный формат Email!";
+            echo "<br>Неверный формат Email!";
         }
-      ?>
-    </p>
-    <p>
-      <label>Ваш пароль:<br></label>
-      <input name="password" type="password" size="15" maxlength="15">
-    </p>
-    <!--**** В поле для паролей (name="password" type="password") пользователь вводит свой пароль ***** -->
-    <p>
-      <input type="submit" name="submit" value="Зарегистрироваться">
-      <!--**** Кнопочка (type="submit") отправляет данные на страничку save_user.php ***** -->
-    </p>
-  </form>
+        ?>
+        <div class="item">
+            <input class="field" name="password" type="password" size="15" maxlength="15" placeholder="Ваш пароль">
+        </div>
+        <!--**** В поле для паролей (name="password" type="password") пользователь вводит свой пароль ***** -->
+        <div class="item">
+            <input type="submit" name="submit" value="Зарегистрироваться" class="but">
+            <!--**** Кнопочка (type="submit") отправляет данные на страничку save_user.php ***** -->
+        </div>
+    </form>
+</div>
 </body>
 
 </html>
