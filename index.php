ec3fbbe2b38d3760d2b27b861e5570552299285a
Alexandr <kataisk2002@gmail.com>
2022-12-23 22:22:31 +0500

    1.0

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <html lang="ru">
 
 <meta charset="utf-8">
+<head>
+    <title>Регистрация</title>
+    </head>
+    <body>
+    <h2>Регистрация</h2>
+    <form action="save_user.php" method="post">
+    <!--**** save_user.php - это адрес обработчика.  То есть, после нажатия на кнопку "Зарегистрироваться", данные из полей  отправятся на страничку save_user.php методом "post" ***** -->
+<p>
+    <label>Ваш логин:<br></label>
+    <input name="login" type="text" size="15" maxlength="15">
+    </p>
+<!--**** В текстовое поле (name="login" type="text") пользователь вводит свой логин ***** -->
+<p>
+    <label>Ваш пароль:<br></label>
+    <input name="password" type="password" size="15" maxlength="15">
+    </p>
+<!--**** В поле для паролей (name="password" type="password") пользователь вводит свой пароль ***** -->
+<p>
+    <input type="submit" name="submit" value="Зарегистрироваться">
+<!--**** Кнопочка (type="submit") отправляет данные на страничку save_user.php ***** -->
+</p></form>
+    </body>
+</html>
+/*
 <?php
   require_once('src/php/login.php');
   $connection = new mysqli($hostname, $username, $password, $database);
@@ -48,4 +72,4 @@
       $string = stripslashes($string);
     return $connection->real_escape_string($string);
   }
-?>
+?> */
